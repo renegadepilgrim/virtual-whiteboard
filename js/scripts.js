9beebe11e6c47cc37e10f1526192c6a26d64c415
slav4477 <slav4477@users.noreply.github.com>
2016-10-31 20:03:40 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// business logic
+function Event(title, time, location, meetupLocation, description) {
+  this.eventTitle = title;
+  this.eventTime = time;
+  this.eventLocation = location;
+  this.meetupLocation = meetupLocation;
+  this.eventDescription = description;
+}
+
+function Creator(creator, creatorContact) {
+  this.creatorName = creator;
+  this.creatorContact = creatorContact;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("#HOMEPAGE form").submit(function(event) {
+    event.preventDefault();
+
+    var eventTitle = $("#EVENTTITLE").val();
+    var eventTime = $("").val();
+    var eventLocation = $("").val();
+    var meetupLocation = $("").val();
+    var eventDescription = $("").val();
+
+    var creatorName = $("").val();
+    var creatorContact = $("").val();
+
+    $("#SHOW").text(event1.eventTitle);
+
+    var event1 = new Event(eventTitle, eventTime, eventLocation, meetupLocation, eventDescription);
+    var creator1 = new Creator(creatorName, creatorContact);
+  });
+});
